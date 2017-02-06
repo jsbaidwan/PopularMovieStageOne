d56713cb9ef073d71a165880ce5152d74ad19483
jsbaidwan <jsbaidwan5@gmail.com>
2017-02-06 23:13:04 +0530

    Initial commit

---
+++ app/src/main/java/com/example/android/popularmoviestageone/ImageAdapter.java
@@ -0,0 +1,41 @@
+package com.example.android.popularmoviestageone;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+
+import java.util.List;
+
+/**
+ * Created by jsbaidwan on 04/02/17.
+ */
+public class ImageAdapter extends ArrayAdapter<Movie> {
+
+    private static final String LOG_TAG = ImageAdapter.class.getSimpleName();
+
+    /* Constructor to instantiate ImageAdpater   */
+    public ImageAdapter(Activity context, List<Movie> movieList)    {
+
+        super(context, 0, movieList);
+    }
+
+    /* Return number of cells*/
+    @Override
+
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Movie movie = getItem(position);
+
+        if(convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.list_item_movie, parent, false);
+       }
+        ImageView moviePoster = (ImageView) convertView.findViewById(R.id.list_item_movieImage);
+        moviePoster.setImageResource(Movie.image);
+        return convertView;
+    }
+}
---
+++ app/src/main/java/com/example/android/popularmoviestageone/MainActivityFragment.java
@@ -0,0 +1,45 @@
+package com.example.android.popularmoviestageone;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+
+import java.util.Arrays;
+
+/**
+ * Created by jsbaidwan on 06/02/17.
+ */
+
+public class MainActivityFragment extends Fragment {
+
+    private ImageAdapter movieImageAdapter;
+
+    Movie [] movieData = {
+            new Movie(R.drawable.a)
+
+
+    };
+
+    public MainActivityFragment()   {
+
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.activity_main, container, false);
+
+        movieImageAdapter = new ImageAdapter(getActivity(), Arrays.asList(movieData));
+
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+        gridView.setAdapter(movieImageAdapter);
+
+        return rootView;
+
+    }
+}
